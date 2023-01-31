3b8a6b063420c5a30a9705c47f64c40b79f01d7d
q1002875 <fulljeffxu@gmail.com>
2023-01-31 10:19:48 +0800

    first commit

--- lib/question.dart
+++ lib/question.dart
@@ -5,7 +5,7 @@ class Question extends StatelessWidget {
 
 final String questionText;
 
-Question(this.questionText);
+const Question(this.questionText);
   @override
   Widget build(BuildContext context) {
     return Container(width: double.infinity,
